c541e96df0e4198e2c9aac4561cb38bf478a15fe
sonvq <vuquangson1610@gmail.com>
2016-08-25 15:36:24 +0700

    commit fix response

--- app/controllers/EventController.php
+++ app/controllers/EventController.php
@@ -164,7 +164,7 @@ class EventController extends BaseController {
         $eventObject->common_interests = $arrayInterest;
         //$object->event_type_details = EventType::find($object->event_type)->toArray();
                 
-		return $eventObject->toArray();
+        return ApiResponse::json(Helper::successResponseFormat(null, $eventObject->toArray()));
 	}
 
 	public function missingMethod( $parameters = array() )
